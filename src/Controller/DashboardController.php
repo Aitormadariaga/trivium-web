4748facd57cba675b241f1dff09b9419d67eea58
Aitormadariaga <amadariagal@a.mikeldi.com>
2026-03-12 14:04:01 +0100

    Creación proyecto

---
+++ src/Controller/DashboardController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class DashboardController extends AbstractController
+{
+    #[Route('/', name: 'dashboard')]
+    public function index(): Response
+    {
+        return $this->render('dashboard/index.html.twig', [
+            'totalPacientes' => 0,
+            'totalSesiones' => 0,
+            'totalUsuarios' => 0,
+            'dispositivosActivos' => 0,
+            'nuevosEsteMes' => 0,
+            'sesionesEsteMes' => 0,
+            'ultimasSesiones' => [],
+            'pacientesRecientes' => [],
+        ]);
+    }
+}
